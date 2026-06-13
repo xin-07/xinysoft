b7120983bab7d15e35de18a33315d3c7ce0ebdb6
xiny <2074835619@qq.com>
2026-06-13 18:17:54 +0800

    修改访问的接口

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const apiTarget = env.development.VITE_API_TARGET || env.production.VITE_API_TARGET
+  const apiTarget = env.VITE_API_TARGET
 
   return {
     plugins: [vue()],
